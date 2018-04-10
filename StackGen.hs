6e77ca414d3b731691889dd058fc2ab183f585fe
Michael <mikegeeraert@gmail.com>
2018-04-10 13:04:02 -0600

    fill out if and while statements

--- StackGen.hs
+++ StackGen.hs
@@ -3,15 +3,15 @@ module StackGen where
 
 import IR
 
-
+type LabelNumber = Int
 
 codeGeneration:: I_prog -> String
 codeGeneration (IPROG(funs, varsize, arraySpecs, stmts)) = progCode
     where
         progStartCode = prog_start varsize
-        progStmtCode  = code_stmts stmts
+        (progStmtCode, n)  = code_stmts 0 stmts
         progFinishCode = prog_finish varsize
-        progFunCode = code_funs funs
+        (progFunCode, n') = code_funs n funs
         progCode = progStartCode ++ progStmtCode ++ progFinishCode ++ progFunCode
 
 prog_start:: Int -> String
@@ -26,17 +26,21 @@ prog_finish varsize = "\t% -- end program --\n" ++
                         "\tALLOC -" ++ show varsize ++ "\n" ++ 
                         "\tHALT\n"
 
-code_funs:: [I_fbody] -> String
-code_funs [] = ""
-code_funs (fun:rest) = (code_fun fun ++ code_funs rest)
+code_funs:: LabelNumber -> [I_fbody] -> (String, LabelNumber)
+code_funs n [] = ("", n)
+code_funs n (fun:rest) = result 
+ where
+    (funCode, n') = code_fun n fun 
+    (funsCode, n'') = code_funs n'' rest
+    result = (funCode ++ funsCode, n'')
 
-code_fun:: I_fbody -> String
-code_fun (IFUN(label, funs, varsize, argsize, arraySpecs, stmts)) = funCode
+code_fun:: LabelNumber -> I_fbody -> (String, LabelNumber)
+code_fun n (IFUN(label, funs, varsize, argsize, arraySpecs, stmts)) = (funCode, n'')
     where
         funStartCode = fun_start label varsize 
-        funStmtCode = code_stmts stmts
+        (funStmtCode, n') = code_stmts n stmts
         funFinishCode = fun_finish argsize varsize
-        nestedFunCode = code_funs funs
+        (nestedFunCode, n'') = code_funs n' funs
         funCode = funStartCode ++ funStmtCode ++ funFinishCode ++ nestedFunCode
 
 
@@ -64,30 +68,63 @@ fun_finish argsize varsize  =  "\t% -- return from function --\n" ++
                         "\tJUMP_S\n"
 
 
-code_stmts:: [I_stmt] -> String
-code_stmts [] = ""
-code_stmts (stmt:rest) = (code_stmt stmt ++ code_stmts rest)
+code_stmts:: LabelNumber -> [I_stmt]  -> (String, LabelNumber)
+code_stmts n [] = ("", n)
+code_stmts n (stmt:rest) = result
+    where
+        (stmtcode, n') = code_stmt n stmt 
+        (stmtscode, n'') = code_stmts n' rest
+        result = ((stmtcode ++ stmtscode), n'')
 
-code_stmt:: I_stmt -> String
-code_stmt (IASS(level, offset, arraySpecs, expr)) = code_expr expr ++ 
-                                                    code_pointer level ++
-                                                    "\tSTORE_O" ++ show offset "\n"
+code_stmt:: LabelNumber -> I_stmt -> (String, LabelNumber)
+code_stmt n (IASS(level, offset, arraySpecs, expr)) = (code_expr expr ++ 
+                                                       code_pointer level ++
+                                                       "\tSTORE_O" ++ show offset ++ "\n",
+                                                       n)
 
+code_stmt n (IWHILE(expr, stmt)) = (code, n''')
+    where
+        (endLabel, n') = (("endwhile_" ++ show n), n+1)
+        (guardLabel, n'') = (("while_" ++ show n'), n'+1)
+        (stmtCode, n''') = code_stmt n'' stmt
+        exprCode = code_expr expr
+        code = "\t% -- while loop --\n" ++
+               guardLabel ++ ":" ++ exprCode ++
+               "\tJUMP_C " ++ endLabel ++ "\n" ++
+               stmtCode ++
+               "\tJUMP " ++ guardLabel ++ "\n" ++
+               endLabel ++ ":\n"
+code_stmt n (ICOND(expr, stmt1, stmt2)) = (code, n'''')
+    where 
+        (elseLabel, n') = (("else_" ++ show n), n+1)
+        (endLabel, n'') = (("endif_" ++ show n''), n'+1)
+        (stmt1Code, n''') = code_stmt n'' stmt1
+        (stmt2Code, n'''') = code_stmt n''' stmt2
+        exprCode = code_expr expr
+        code = "\t% -- conditional statement --\n" ++
+               exprCode ++
+               "\tJUMP_C " ++ elseLabel ++ "\n" ++
+               stmt1Code ++
+               "\tJUMP " ++ endLabel ++ "\n" ++
+               elseLabel ++ ":" ++ stmt2Code ++
+               endLabel ++ ":\n"
 
-code_stmt (IWHILE(expr, stmt)) = ""
-code_stmt (ICOND(expr, stmt1, stmt2)) = ""
-code_stmt (IREAD_F(level, offset, exprs)) = ""
-code_stmt (IREAD_I(level, offset, exprs)) = ""
-code_stmt (IREAD_B(level ,offset, exprs)) = ""
-code_stmt (IPRINT_F expr) = ""
-code_stmt (IPRINT_I expr) = ""
-code_stmt (IPRINT_B expr) = ""
-code_stmt (IRETURN  expr) = ""
-code_stmt (IBLOCK(funs, varsize, arraySepcs, stmts)) = ""
+code_stmt n (IREAD_F(level, offset, exprs)) = (code, n)
+    where
+        exprsCode = code_exprs exprs
+        code = ""
+
+code_stmt n (IREAD_I(level, offset, exprs)) = ("", n)
+code_stmt n (IREAD_B(level ,offset, exprs)) = ("", n)
+code_stmt n (IPRINT_F expr) = ("", n)
+code_stmt n (IPRINT_I expr) = ("", n)
+code_stmt n (IPRINT_B expr) = ("", n)
+code_stmt n (IRETURN expr) = ("", n)
+code_stmt n (IBLOCK(funs, varsize, arraySepcs, stmts)) = ("", n)
 
 code_pointer:: Int -> String
 code_pointer level = "\tLOAD_R %fp\n" ++ 
-                     (concat $ replicate lv "\tLOAD_O -2 \n")
+                     (concat $ replicate level "\tLOAD_O -2 \n")
 
 code_exprs:: [I_expr] -> String
 code_exprs [] = ""
